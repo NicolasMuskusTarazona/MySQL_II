06d6971dd7d6c66d4ea0969e52c909176effc795
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-06-13 08:38:23 -0500

    feat: :sparkles: `Datos separador`.
    Datos.SQL

--- Datos.sql
+++ Datos.sql
@@ -27,3 +27,15 @@ INSERT INTO `factura`(orden_id,fecha,total,estado) VALUES
 (1,'2025-01-15',12.00,'pendite'),
 (2,'2025-05-30',21.00,'realizado'),
 (3,'2025-02-06',3.00,'cancelado')
+
+INSERT INTO `separador` (precio, Color) VALUES
+(5.50, 'rojo'),
+(3.99, 'azul'),
+(4.25, 'verde'),
+(6.75, 'negro'),
+(2.50, 'blanco'),
+(7.80, 'morado'),
+(1.99, 'amarillo'),
+(8.30, 'rosado'),
+(3.40, 'naranja'),
+(4.75, 'gris');
